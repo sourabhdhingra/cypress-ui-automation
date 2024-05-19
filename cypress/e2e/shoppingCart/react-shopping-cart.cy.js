a36fe3b0653ccb0f28a9919fce7c832ce32a403c
sourabhdhingra <sourabhdhingra91@gmail.com>
2024-05-19 15:07:57 +0530

    test case to buy S size shirt, github action workflow for cypress run

--- cypress/e2e/shoppingCart/react-shopping-cart.cy.js
+++ cypress/e2e/shoppingCart/react-shopping-cart.cy.js
@@ -1,6 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Shopping Cart', () => {
+
+  beforeEach(() => {
+    // visit the react shopping cart page
     cy.visit('https://react-shopping-cart-67954.firebaseapp.com/')
+  });
+  
+  
+  it('should be able to buy Black Batman T-shirt', () => {
     cy.get('.checkmark:contains("S")').click({ multiple: true })
+    // cy.get('img[alt="Black Batman T-shirt').trigger('mouseover')
+
+    cy.get('.sc-bj2vay-1:nth-child(3) .checkmark').click();
+    // cy.get('[data-testid=checkbox]').click();
+    // cy.get('.kaJsVi > .sc-124al1g-0').click();
+    // cy.get('.sc-1h98xa9-11').click();
+
   })
 })
